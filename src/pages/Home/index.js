5256b40ce52de263201b643d9f1c5220212b55a7
ArtSantana <artsantana1457@hotmail.com>
2020-09-29 23:33:07 -0300

    Setup background

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
+import {StatusBar} from 'react-native';
+import {Container} from './styles';
+import colors from '../../assets/colors';
 
 function Home() {
-  return <></>;
+  useEffect(() => {
+    navigationColor();
+  }, []);
+
+  async function navigationColor() {
+    try {
+      await changeNavigationBarColor(colors.primary, false);
+    } catch (e) {}
+  }
+
+  return (
+    <Container>
+      <StatusBar backgroundColor={colors.primary} barStyle="light-content" />
+    </Container>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import colors from '../../assets/colors';
+
+const Container = styled.View`
+  background-color: ${colors.primary};
+  width: 100%;
+  height: 100%;
+`;
+
+export {Container};
